70583bbd933ee34f0ecafc4298b17d48c6ad5bf7
Søren Bjergmark <sahb1239@gmail.com>
2018-03-21 17:30:38 +0100

    Added page in GraphQL backend

---
+++ src/Backend/GraphQL/Types/PageGraphType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Backend.Model;
+using GraphQL.Types;
+
+namespace Backend.GraphQL.Types
+{
+    public class PageGraphType : ObjectGraphType<Page>
+    {
+        public PageGraphType()
+        {
+            Field(e => e.Url).Description("Contains the url for the current subpage");
+            Field(e => e.Content).Description("Contains the content of the page");
+        }
+    }
+}
---
+++ src/Backend/Repositories/IPageRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Backend.Model;
+
+namespace Backend.Repositories
+{
+    public interface IPageRepository
+    {
+        Task<IEnumerable<Page>> GetPages();
+        Task<Page> GetPage(string url);
+    }
+}
